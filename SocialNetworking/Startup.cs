d3439f8fd4d3d3368c8430d670919219754e776e
pedroT <pedrotavares41@gmail.com>
2017-03-18 15:06:54 +0000

    Add project files.

---
+++ SocialNetworking/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialNetworking.Startup))]
+
+namespace SocialNetworking
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
